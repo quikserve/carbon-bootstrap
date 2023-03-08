22a0c9bd94e315c5d458446ad0aff08ba0a58ea6
Chris Marquis <chris@martifex.com>
2023-03-08 16:20:59 -0600

    Updates

--- ubuntu/scripts/bootstrap.sh
+++ ubuntu/scripts/bootstrap.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-sudo touch /home/quikserve/test.txt
+echo "QuikServe Solutions"
+echo "Setup will now begin..."
 
 # Detect hardware
 
 # Install base dependencies
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y ubuntu-desktop-minimal
 
 # Setup security updates automatic
 
 # Install/configure hardware specific dependencies
 
 # Setup pos user
-  # auto login
+sudo useradd pos
+echo "pos:pos" | sudo chpasswd
+sudo usermod -a -G nopasswdlogin pos
+echo "auth sufficient pam_succeed_if.so user ingroup nopasswdlogin" | sudo tee -a /etc/pam.d/gdm-password
 
 # SSH only for quikserve user with key
 
