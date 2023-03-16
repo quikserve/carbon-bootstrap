63249548e996d4a5001f3a75c4b3015305dd2634
Chris Marquis <chris@martifex.com>
2023-03-16 16:44:33 -0500

    updates

--- ubuntu/scripts/post_login.sh
+++ ubuntu/scripts/post_login.sh
@@ -6,6 +6,7 @@
 gsettings set org.gnome.desktop.session idle-delay 0
 gsettings set org.gnome.desktop.interface color-scheme prefer-dark
 
+# TODO could determine if a) there is a second monitor and b) its port and c) where to put it
 xrandr --output DP-1 --pos 0x1080
 
 x0vncserver --passwordfile ~/.vnc/passwd -display: 0 --localhost no
